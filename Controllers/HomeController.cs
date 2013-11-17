8a4a37254e8c0fbe8512988c4ad9f004159da249
dawu17 <bluedaniel86@hotmail.com>
2013-11-17 13:40:38 -0800

    First upload without change

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS445.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormatPhoneNumber(string inputNumber)
+        {
+
+          if (string.IsNullOrEmpty(inputNumber))
+          {
+            return Content("invalid input, please try again");
+          }
+          else
+          {
+            string formatted = formatNumber(inputNumber);
+            return View((object)formatted);
+          }
+        }
+
+        // please implement your phone number formatting logic here
+        private string formatNumber(string inputNumber)
+        {
+          // imiplement formatting here
+          return "formated number is: " + inputNumber;
+        }
+
+    }
+}
